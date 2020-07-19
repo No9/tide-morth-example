5d68201f509697fe42b7753215111116a75059c5
Anton Whalley <anton@venshare.com>
2020-07-19 02:14:40 +0100

    add show and new forms

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,17 @@ async fn main() -> Result<(), std::io::Error> {
 
     let mut articles = app.at("/articles");
     articles.get(routes::articles::index);
+
+    articles.at("/new")
+    .get(routes::articles::new)
+    .post(routes::articles::create);
+    articles
+        .at("/:article_id")
+        .get(routes::articles::show);
+        // .delete(routes::articles::delete)
+        // .put(routes::articles::update)
+        
+
     app.at("/public").serve_dir("public/")?;
     app.listen("127.0.0.1:8080").await?;
     Ok(())
--- src/routes/articles.rs
+++ src/routes/articles.rs
@@ -5,6 +5,9 @@ use handlebars::to_json;
 use serde_json::value::Map;
 use tide::Request;
 use tide_mongodb_dal;
+use mongodb::{
+    bson::{Document},
+};
 
 pub async fn index(req: Request<State>) -> tide::Result {
     let state = &req.state();
@@ -19,9 +22,60 @@ pub async fn index(req: Request<State>) -> tide::Result {
     };
     let hb = &state.registry;
     let mut data = Map::new();
-    data.insert("title".to_string(), to_json("hello tide!"));
+    data.insert("title".to_string(), to_json("Articles"));
     data.insert("parent".to_string(), to_json("layouts/main"));
 
     data.insert("articles".to_string(), to_json(&docs));
     Ok(hb.render_response_ext("articles/list", &data, "html")?)
+}
+
+pub async fn show(req: Request<State>) -> tide::Result {
+    let state = &req.state();
+
+    let client = &state.client;
+    let id = req.param::<String>("article_id")?;
+    //let id = req.param("article_id")?;
+
+    let doc = match tide_mongodb_dal::get_by_id(client, "test", "articles", &id).await {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Error: {}", e);
+            Document::new()
+        }
+    };
+    let hb = &state.registry;
+    let mut data = Map::new();
+    data.insert("title".to_string(), to_json("Articles"));
+    data.insert("parent".to_string(), to_json("layouts/main"));
+
+    data.insert("articles".to_string(), to_json(&doc));
+    Ok(hb.render_response_ext("articles/show", &data, "html")?)
+}
+
+pub async fn new(req: Request<State>) -> tide::Result {
+    let hb = &req.state().registry;
+    let mut data = Map::new();
+    data.insert("title".to_string(), to_json("New Article"));
+    data.insert("parent".to_string(), to_json("layouts/main"));
+    data.insert("action".to_string(), to_json("/articles/new"));
+    Ok(hb.render_response_ext("articles/form", &data, "html")?)
+}
+
+pub async fn create(mut req: Request<State>) -> tide::Result {
+    let hb = &req.state().registry;
+    let mut data = Map::new();
+    data.insert("title".to_string(), to_json("New Article Reponse"));
+    data.insert("parent".to_string(), to_json("layouts/main"));
+    Ok(hb.render_response_ext("articles/form", &data, "html")?)
+    // let db = &request.state().db;
+    // let mut tx = db.begin().await?;
+    // let article: PartialArticle = utils::deserialize_body(&mut request).await?;
+    // let created = article.create().execute(&mut tx).await?;
+
+    // if created == 1 {
+    //     let (last_id,) = Article::last_id().fetch_one(&mut tx).await?;
+    //     Ok(tide::Redirect::new(format!("/articles/{}", last_id)).into())
+    // } else {
+    //     Ok(ArticleForm::for_partial_article(&article).into())
+    // }
 }
